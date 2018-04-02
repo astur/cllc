94eae18c6372548e8ce0bc290f0fbff3dcfac86d
astur <astur@yandex.ru>
2018-04-02 03:53:21 +0300

    refactorings

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 
 chalk.enabled = !!process.stdout.isTTY;
 
-const levels = {
+const _levels = {
     trace: chalk.white.bold.bgBlack('<TRACE>'),
     debug: chalk.white.bold.bgGreen('<DEBUG>'),
     info: chalk.white.bold.bgBlue('<INFO>'),
@@ -14,47 +14,42 @@ const levels = {
 let _i = [];
 let _text = '';
 let _visible = false;
-let _counting = false;
 
-function _countStr(){
-    let t = _text;
-    for(let i = 0; i < _i.length; i++){
-        t = t.replace(/%s/, chalk.white(_i[i]));
-    }
-    return t;
-}
+const _countStr = () => _text
+    .split(/%s/)
+    .map((v, i) => v + (_i[i] ? chalk.white(_i[i]) : ''))
+    .join('');
 
-function L(tag){
+module.exports = function(tag){
     let dateFormat = '%T';
     let level;
 
-    const _show = function(){
+    const _show = () => {
         process.stdout.write(`${_countStr()}\r`);
         _visible = true;
     };
 
-    const _hide = function(){
+    const _hide = () => {
         process.stdout.write(`${' '.repeat(_countStr().length)}\r`);
         _visible = false;
     };
 
-    const _log = function(l){
-        return function(...args){
-            const a = [];
-            const ll = levels[l] || levels[level];
-            if(dateFormat) a.push(chalk.white(`[${strftime(dateFormat)}]`));
-            if(ll) a.push(ll);
-            if(tag) a.push(chalk.cyan(`(${tag})`));
-            a.push(...args.map(v => chalk.gray(v)));
+    const _log = l => (...args) => {
+        const a = [];
+        const ll = _levels[l] || _levels[level];
 
-            if(_visible){
-                _hide();
-                console.log.apply(null, a);
-                _show();
-            } else {
-                console.log.apply(null, a);
-            }
-        };
+        if(dateFormat) a.push(chalk.white(`[${strftime(dateFormat)}]`));
+        if(ll) a.push(ll);
+        if(tag) a.push(chalk.cyan(`(${tag})`));
+        a.push(...args.map(v => chalk.gray(v)));
+
+        if(_visible){
+            _hide();
+            console.log(...a);
+            _show();
+        } else {
+            console.log(...a);
+        }
     };
 
     const log = _log();
@@ -65,60 +60,53 @@ function L(tag){
     log.w = log.warn = _log('warn');
     log.e = log.error = _log('error');
 
-    log.start = function(text = '%s', ...args){
-        if(!process.stdout.isTTY) return;
-        _counting = true;
-        _i = args;
-        _text = text;
-        const tl = _text.split(/%s/).length - 1 || 1;
-        if(tl > _i.length) _i = _i.concat(Array(tl).fill(0));
-        _show();
+    log.level = l => {
+        level = _levels[l] ? l : undefined;
     };
 
-    log.step = function(...args){
-        if(_counting){
-            args = args.length ? args : [1];
-            for(let i = 0; i < _i.length; i++){
-                _i[i] += +args[i] || 0;
-            }
-            _show();
-        }
-    };
-
-    log.stop = function(){
-        if(!process.stdout.isTTY) return;
-        _counting = false;
-        _hide();
-    };
-
-    log.finish = function(text = _text, ...args){
-        if(_counting){
-            _counting = false;
-            _hide();
-            _text = text;
-            for(let i = 0; i < _i.length; i++){
-                if(typeof args[i] === 'number') _i[i] = args[i];
-            }
-            console.log(_countStr());
-        }
-    };
-
-    log.level = function(l){
-        level = Object.keys(levels).indexOf(l) === -1 ? undefined : l;
-    };
-
-    log.dateFormat = function(dF){
+    log.dateFormat = dF => {
         dateFormat = typeof dF === 'string' || !dF ? dF : dateFormat;
     };
 
-    log.tag = function(t){
+    log.tag = t => {
         tag = t && t.id && t.exports && t.filename && t.paths ?
             t.filename.split(/[\\/]/).slice(-2).join('/').split('.')[0] :
             typeof t === 'string' ? t : undefined;
     };
 
     log.tag(tag);
-    return log;
-}
 
-module.exports = L;
+    log.start = (text = '%s', ...args) => {
+        if(!process.stdout.isTTY || !text || typeof text !== 'string') return;
+        _text = text;
+        const tl = _text.split(/%s/).length - 1 || 1;
+        args = args.length ? args.map(v => +v || 0) : [1];
+        _i = Object.assign(Array(tl).fill(0), args.slice(0, tl));
+        _show();
+    };
+
+    log.step = (...args) => {
+        if(!_text) return;
+        args = args.length ? args : [1];
+        _i = _i.map((v, i) => v + (+args[i] || 0));
+        _show();
+    };
+
+    log.stop = () => {
+        if(!process.stdout.isTTY) return;
+        _text = '';
+        _hide();
+    };
+
+    log.finish = (text = _text, ...args) => {
+        if(!_text) return;
+        _hide();
+        _text = text;
+        args = args.length ? args.map(v => +v || 0) : [];
+        _i = Object.assign(_i, args.slice(0, _i.length));
+        console.log(_countStr());
+        _text = '';
+    };
+
+    return log;
+};
