de0257055b080d8f8b9804d73db7d7ba69525589
astur <astur@yandex.ru>
2018-06-15 10:08:14 +0300

    test refactoring

--- test.js
+++ test.js
@@ -19,9 +19,7 @@ test('log', t => {
     log('TEST1', 'TEST2');
     inspect.restore();
     t.is(inspect.output[0].replace(re, ''), 'TEST\n');
-    t.is(inspect.output[0].match(re)[0], styles.color.gray.open);
-    t.is(inspect.output[0].match(re)[1], styles.color.gray.close);
     t.is(inspect.output[1].replace(re, ''), 'TEST1 TEST2\n');
-    t.is(inspect.output[1].match(re)[0], styles.color.gray.open);
-    t.is(inspect.output[1].match(re)[1], styles.color.gray.close);
+    t.deepEqual(inspect.output[0].match(re), [styles.color.gray.open, styles.color.gray.close]);
+    t.deepEqual(inspect.output[1].match(re), [styles.color.gray.open, styles.color.gray.close]);
 });
