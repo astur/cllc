7d3c9d182a628bcdde58e04a777ea774aba12358
astur <astur@yandex.ru>
2018-06-15 09:52:28 +0300

    test for simple log

--- test.js
+++ test.js
@@ -1,7 +1,27 @@
 const test = require('ava');
-const m = require('.');
+const cllc = require('.');
+const stdout = require('test-console').stdout;
+const re = require('ansi-regex')();
+const styles = require('ansi-styles');
 
-test('main', t => {
-    t.pass();
-    t.is(m, m);
+test('empty', t => {
+    const inspect = stdout.inspect();
+    const log = cllc(null, null);
+    log();
+    inspect.restore();
+    t.is(inspect.output[0], '\n');
+});
+
+test('log', t => {
+    const inspect = stdout.inspect();
+    const log = cllc(null, null);
+    log('TEST');
+    log('TEST1', 'TEST2');
+    inspect.restore();
+    t.is(inspect.output[0].replace(re, ''), 'TEST\n');
+    t.is(inspect.output[0].match(re)[0], styles.color.gray.open);
+    t.is(inspect.output[0].match(re)[1], styles.color.gray.close);
+    t.is(inspect.output[1].replace(re, ''), 'TEST1 TEST2\n');
+    t.is(inspect.output[1].match(re)[0], styles.color.gray.open);
+    t.is(inspect.output[1].match(re)[1], styles.color.gray.close);
 });
